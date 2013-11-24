4dd216a4b86b576d12ea1931f43e632f44be65b5
Stephen Punwasi <stephen.punwasi@gmail.com>
2013-11-24 12:38:47 -0500

    Modified For r/Documentaries
    -Modified the reddit video plugin to work specifically on
    r/documentaries
    -Removed extra plugins

--- script.hululibraryautoupdate/autoexec.py
+++
@@ -1,7 +0,0 @@
-import xbmc
-
-#use this if you are running Weather+ to avoid a startup conflict
-#xbmc.executebuiltin('AlarmClock(huluupdatelibrary,XBMC.RunScript(script.hululibraryautoupdate),1,true)')
-
-#otherwise this one should be fine
-xbmc.executebuiltin('RunScript(script.hululibraryautoupdate)')
